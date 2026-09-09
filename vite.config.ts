9afb1e964f242206b5fb6e8c8ec730dafffe3420
jenni.hyttinen@gmail.com <jenni.hyttinen@gmail.com>
2026-09-09 14:26:24 +0300

    Add PWA support and app icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,33 @@
 import react from '@vitejs/plugin-react'
-
 import { defineConfig } from 'vite'
-
-// https://vite.dev/config/
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-
-  plugins: [react()],
-
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Where Did I Put That?',
+        short_name: 'Where Did I Put That?',
+        description: 'Save where you put things so you can find them later.',
+        theme_color: '#f88c8c',
+        background_color: '#faf6f0',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/where-did-i-put-that/APP-ICON-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/where-did-i-put-that/APP-ICON-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   base: '/where-did-i-put-that/',
-
 })
